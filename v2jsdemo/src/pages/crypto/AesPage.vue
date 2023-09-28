65265891ab66a77f5815fee5f2bf939a78e4228b
chaosannals <chaosannals@outlook.com>
2023-09-28 14:53:50 +0800

    save

--- v2jsdemo/src/pages/crypto/AesPage.vue
+++ v2jsdemo/src/pages/crypto/AesPage.vue
@@ -1,28 +1,35 @@
 <template>
     <div class="aes-page">
         <input type="file" @input="onLoadFile" />
-        <input type="text" v-model="resultMd5Hex" />
-        <input type="text" v-model="resultMd5Base64" />
+        <div>size: {{ fileSize }}</div>
+        <div>buffer size: {{ bufferSize }}</div>
+        <textarea v-model="resultMd5Hex" rows="4"></textarea>
+        <textarea v-model="resultMd5Base64" rows="4"></textarea>
     </div>
 </template>
 
 <script>
-import md5 from 'crypto-js/md5';
+import CryptoJS from 'crypto-js';
 import Hex from 'crypto-js/enc-hex';
 import Base64 from 'crypto-js/enc-base64';
+import md5 from 'crypto-js/md5';
 import { readAsArrayBuffer } from '../../utils/io';
 
 export default {
     data: () => ({
+        fileSize: 0,
+        bufferSize: 0,
         resultMd5Hex: "",
         resultMd5Base64: "",
     }),
     methods: {
         async onLoadFile(e) {
-            console.log(e);
+            console.log('event', e);
             let file = e.target.files[0];
             let buffer = await readAsArrayBuffer(file);
-            let result = md5(buffer);
+            this.fileSize = file.size;
+            this.bufferSize = buffer.byteLength;
+            let result = md5(CryptoJS.lib.WordArray.create(buffer));
             this.resultMd5Base64 = Base64.stringify(result);
             this.resultMd5Hex = Hex.stringify(result);
         }
