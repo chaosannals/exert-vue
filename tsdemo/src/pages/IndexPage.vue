7c5e892e6fb93eaa2aa66180a4f745bc9d7282b9
chaosannals <chaosannals@outlook.com>
2023-10-04 10:28:50 +0800

    save

---
+++ tsdemo/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="index-page">
+        <div v-for="link in links" @click="onClickLink(link)">
+            <span>{{ link }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {routes} from '../router';
+import { useRouter } from 'vue-router';
+import { computed } from 'vue';
+
+const $router = useRouter();
+const links = computed(() => {
+    return routes.map(i => i.path);
+});
+const onClickLink = (link) => {
+    $router.push(link);
+};
+
+</script>
