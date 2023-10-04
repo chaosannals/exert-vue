451e3c99bc6b4aceb7d1c8d9de916da1ca767f15
chaosannals <chaosannals@outlook.com>
2023-10-04 10:13:26 +0800

    save

--- jsdemo/src/pages/IndexPage.vue
+++ jsdemo/src/pages/IndexPage.vue
@@ -9,15 +9,18 @@
 <script setup>
 import { computed, reactive } from 'vue';
 import { routes } from '../router';
-import router from '../router';
+import { useRouter } from 'vue-router';
+// import router from '../router'; // 此句和 useRouter 一样，不过按照惯例，使用 useRouter 。
 
 const data = reactive({});
 const links = computed(() => {
     return routes.map(i => i.path);
 });
 
+const $router = useRouter();
+
 const onClickLink = (link) => {
-    router.push(link);
+    $router.push(link);
 };
 
 </script>
