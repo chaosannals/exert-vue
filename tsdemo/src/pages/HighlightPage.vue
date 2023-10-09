be1a05fa75835439455896d4829987426fd3e984
chaosannals <chaosannals@outlook.com>
2023-10-09 16:14:57 +0800

    save

--- tsdemo/src/pages/HighlightPage.vue
+++ tsdemo/src/pages/HighlightPage.vue
@@ -3,6 +3,7 @@
         <!-- 由于 pre code span 都是行内元素 所以换行会影响到布局，所以要写成单行。 -->
         <pre class="code-block"><code><span v-html="data.jsCode"></span></code></pre>
         <pre class="code-block"><code><span v-html="data.cppCode"></span></code></pre>
+        <pre class="code-block"><code><span v-html="data.markdownCode"></span></code></pre>
     </div>
 </template>
 
@@ -13,6 +14,7 @@ import { onBeforeMount, reactive } from 'vue';
 const data = reactive({
     jsCode: "",
     cppCode: "",
+    markdownCode: "",
 });
 
 const readText = async (url: string) => {
@@ -23,8 +25,10 @@ const readText = async (url: string) => {
 onBeforeMount(async () => {
     const jsCode = await readText("/code/hello_world.js");
     const cppCode = await readText("/code/hello_world.cpp");
+    const markdownCode = await readText("/code/hello_world.md");
     data.jsCode = hljs.highlightAuto(jsCode).value;
     data.cppCode = hljs.highlightAuto(cppCode).value;
+    data.markdownCode = hljs.highlightAuto(markdownCode).value;
 });
 
 </script>
