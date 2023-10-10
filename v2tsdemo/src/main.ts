2e3818bdae84181899f0601e26f2721137b708dd
chaosannals <chaosannals@outlook.com>
2023-10-10 17:52:32 +0800

    save

--- v2tsdemo/src/main.ts
+++ v2tsdemo/src/main.ts
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import VueRx from 'vue-rx'; // 封装好差的库。
 import App from "./App.vue";
 import router from "./router";
+import { Observable } from 'rxjs'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRx); // 封装好差的库。
 
 new Vue({
   //@ts-ignore
