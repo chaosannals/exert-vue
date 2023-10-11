e250f3942fb35f3b4f8e33b70799b0b3c3f1545e
chaosannals <chaosannals@outlook.com>
2023-10-11 13:42:15 +0800

    save

--- tsdemo/src/pages/IndexPage.vue
+++ tsdemo/src/pages/IndexPage.vue
@@ -3,19 +3,26 @@
         <div v-for="link in links" @click="onClickLink(link)">
             <span>{{ link }}</span>
         </div>
+        <div v-for="f in data.files">
+            <span>{{ f }}</span>
+        </div>
     </div>
 </template>
 
-<script setup>
-import {routes} from '../router';
+<script setup lang="ts">
+import { routes } from '../router';
 import { useRouter } from 'vue-router';
-import { computed } from 'vue';
+import { reactive, computed } from 'vue';
+
+const data = reactive({
+    files: __STATIC_FILES__, // 因为是预定义的变量，无法在 template 里面直接使用，所以这里赋值给 files
+});
 
 const $router = useRouter();
 const links = computed(() => {
     return routes.map(i => i.path);
 });
-const onClickLink = (link) => {
+const onClickLink = (link: string) => {
     $router.push(link);
 };
 
--- tsdemo/vite.config.ts
+++ tsdemo/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readdirSync, statSync } from 'fs';
+// node 相关的模块 url path fs 因为 TS 没有申明，所以需要 @types/node 模块
+
+const here = fileURLToPath(import.meta.url);
+const projectDir = dirname(here);
+const publicDir = join(projectDir, 'public');
+
+function globDir(dirPath): string[] {
+  const result = [];
+  const names = readdirSync(dirPath);
+  for (const name of names) {
+    const path = join(dirPath, name);
+    const stat = statSync(path);
+    if (stat.isFile()) {
+      result.push(path);
+    } else if (stat.isDirectory()) {
+      globDir(path).forEach(i => result.push(i));
+    }
+  }
+  console.log('globDir', result);
+  return result;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  define: {
+    __STATIC_FILES__: globDir(publicDir), // 预设的全局变量需要在 vite-env.d.ts 里面声明类型
+  },
+});
