fd028966553b8962f5408dd0c6f79083afc7d8ce
chaosannals <chaosannals@outlook.com>
2023-10-14 10:35:41 +0800

    save

---
+++ tsdemo/src/pages/proto/JsonProtoPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="json-proto-page">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+import jsonProto from '../../protos/awesome.json';
+import protobuf from 'protobufjs';
+
+const root = protobuf.Root.fromJSON(jsonProto as protobuf.INamespace);
+
+onBeforeMount(() => {
+    console.log('pb', (root  as any).SayJsDemoReply);
+});
+
+</script>
+
+<style scoped lang="scss">
+.json-proto-page {
+
+}
+</style>
