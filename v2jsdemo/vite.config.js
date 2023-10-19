270a05db0ec15a72ae4f006b82c76eb7f328c6dd
chaosannals <chaosannals@outlook.com>
2023-10-19 18:01:48 +0800

    save

--- v2jsdemo/vite.config.js
+++ v2jsdemo/vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
+import { sha512crypt } from 'sha512crypt-node';
+
+const a = '123123';
+const salt = '123123';
+const h1 = sha512crypt(a, salt);
+const h2 = sha512crypt(a, salt);
+
+console.log('hash', h1);
+console.log('hash', h2);
+console.log('hash', h1 === h2);
 
 export default defineConfig({
   plugins: [
