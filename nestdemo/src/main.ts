068a1822bc30b523da3dffc53074f991b0113d69
chaosannals <chaosannals@outlook.com>
2023-10-20 09:23:41 +0800

    save

--- nestdemo/src/main.ts
+++ nestdemo/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { grpcClientOptions } from './grpc-client.options';
+import { join } from 'path';
 
 async function bootstrap() {
+  // 这种写法单纯 微服务
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule, {
+  //     transport: Transport.GRPC,
+  //     options: {
+  //       url: '0.0.0.0:30000',
+  //       package: 'hero',
+  //       protoPath: join(__dirname, 'hero/hero.proto'),
+  //     },
+  //   },
+  // );
+  // await app.listen();
+
+  // 复合写法，可以 多微服务 和 接口
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+
+  await app.startAllMicroservices();
+  await app.listen(3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
