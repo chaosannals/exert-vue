04aa63af9cda9ce3d2deace99d5d1f0e2c0e4888
chaosannals <chaosannals@outlook.com>
2023-10-27 14:09:54 +0800

    save

---
+++ tsdemo/src/pages/ui/PopupDemoPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <button ref="reference" @click="onClick">Button</button>
+    <div ref="floating" :style="floatingStyles">Tooltip</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import {
+    useFloating,
+    offset,
+    flip,
+    shift,
+} from '@floating-ui/vue';
+
+const reference = ref();
+const floating = ref();
+const placement = ref('top');
+const { floatingStyles, update: updateFloating } = useFloating(reference, floating, {
+    transform: true,
+    placement: placement,
+    middleware: [offset(10), flip(), shift()],
+});
+
+const placements = ['top', 'left', 'right', 'bottom'];
+
+const onClick = () => {
+    const index = placements.indexOf(placement.value);
+    console.log('inc', index);
+    placement.value = placements[(index + 1) % placements.length];
+    updateFloating();
+}
+</script>
